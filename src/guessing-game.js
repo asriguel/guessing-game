748e6f904375409c0448a04a6fc651a9499f29c9
a.petrakovich <petrakovich413@gmail.com>
2017-09-14 20:53:07 +0300

    Finished task

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,26 @@
 class GuessingGame {
-    constructor() {}
+    constructor() {
+        this.min;
+        this.max;
+        this.current;
+    }
 
     setRange(min, max) {
-
+        this.min = min;
+        this.max = max;
     }
 
     guess() {
-
+        return this.current = Math.round((this.min + this.max) / 2);
+         
     }
 
     lower() {
-
+        this.max = this.current;
     }
 
     greater() {
-
+        this.min = this.current;
     }
 }
 
